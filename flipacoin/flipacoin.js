da7cf5f1061b1b44dc8279c4094fc92320158118
Adam-GJ <125415462+Adam-GJ@users.noreply.github.com>
2025-06-04 10:43:47 +0100

    Update flipacoin.js

--- flipacoin/flipacoin.js
+++ flipacoin/flipacoin.js
@@ -3,40 +3,47 @@ var currentstate = true;
 
 const ui = {
     buttons: {
-        generate: document.getElementById("generateBtn")
+        generate:           document.getElementById("generateBtn")
     },
     outputs: {
-        coin: document.getElementById("coin"),
-        coin_video: document.getElementById("coin_video")
+        coin:               document.getElementById("coin"),
+        coin_video:         document.getElementById("coin_video"),
+        coin_background:    document.getElementById("coin_background")
     }
 }
 
 ui.buttons.generate.addEventListener("click", () => {
     currentstate = Math.random() < 0.5;
 
+    
+
     if (currentstate) {
         if (laststate) {
-            ui.outputs.coin_video.style.backgroundImage = "url(img/heads.png)";
-            ui.outputs.coin.src = "vid/h2h.mp4";
+            ui.outputs.coin_background.style.backgroundImage = "url(img/heads.png)";
+            ui.outputs.coin.src = "vid/h2h.webm";
         }
         else {
-            ui.outputs.coin_video.style.backgroundImage = "url(img/tails.png)";
-            ui.outputs.coin.src = "vid/t2h.mp4";
+            ui.outputs.coin_background.style.backgroundImage = "url(img/tails.png)";
+            ui.outputs.coin.src = "vid/t2h.webm";
         }
     } else {
         if (laststate) {
-            ui.outputs.coin_video.style.backgroundImage = "url(img/heads.png)";
-            ui.outputs.coin.src = "vid/h2t.mp4";
+            ui.outputs.coin_background.style.backgroundImage = "url(img/heads.png)";
+            ui.outputs.coin.src = "vid/h2t.webm";
         }
         else {
-            ui.outputs.coin_video.style.backgroundImage = "url(img/tails.png)";
-            ui.outputs.coin.src = "vid/t2t.mp4";
+            ui.outputs.coin_background.style.backgroundImage = "url(img/tails.png)";
+            ui.outputs.coin.src = "vid/t2t.webm";
         }
     }
 
     ui.outputs.coin_video.pause();
     ui.outputs.coin_video.currentTime = 0;
     ui.outputs.coin_video.load();
-
+    setTimeout(() => {
+        ui.outputs.coin_background.style.backgroundImage = "url(img/blank.png)";
+    }, 25);
     laststate = currentstate;
 });
+
+ui.outputs.coin_background.style.backgroundImage = "url(img/heads.png)";
